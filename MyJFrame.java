d2ba679a01aeeae014571d9f22fc561897a4e3fc
RomanGCn <143760041+RomanGCn@users.noreply.github.com>
2024-11-05 08:19:15 -0600

    CanoRoman

--- MyJFrame.java
+++ MyJFrame.java
@@ -2,31 +2,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.guiss;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+package sandbox.myjframe;
+
 /**
  *
- * @author efcru
+ * @author Cano Roman
  */
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class MyJFrame extends JFrame {
-    private JLabel myLabel;
-
-    public MyJFrame() {
-        myLabel = new JLabel();
+public class MyJFrame extends JFrame{
+    public JLabel myLabel;
+    
+    public MyJFrame(){
         myLabel.setText("Hola Mundo");
-        add(myLabel);
-        
-        setTitle("Mi Ventana");
-        setSize(300, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        this.add(myLabel);
     }
-
+    
     public static void main(String[] args) {
-        new MyJFrame();
+        MyJFrame frame = new MyJFrame();
+        frame.setSize(300,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        
     }
 }
-
